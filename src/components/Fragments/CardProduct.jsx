df29e03ce24973265abe6dc6fe9079eee5f9b61e
asyifa-mg <asyifamaharanigustina12@gmail.com>
2024-05-03 21:33:07 +0700

    Auth React

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -36,13 +36,12 @@ const Body = (props) => {
   );
 };
 
-const Footer = () => {
+const Footer = (props) => {
+  const { price } = props;
   return (
     <div className="px-5 pb-5">
       <div className="flex items-center justify-between">
-        <span className="text-3xl font-bold text-gray-900 ">
-          $ 100
-        </span>
+        <span className="text-3xl font-bold text-gray-900 ">$ 100</span>
         <a
           href="#"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -5,9 +5,7 @@ const AuthLayouts = (props) => {
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-        <h1 className="text-3xl font-bold mb-2 text-blue-600">
-          {title}
-        </h1>
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
         <p className="font-medium text-slate-500 mb-5">
           Welcome, Please enter your details
         </p>
@@ -32,7 +30,7 @@ const Navigation = ({ type }) => {
     return (
       <p className="text-sm mt-5 text-center">
         Already have an account?{" "}
-        <Link className="font-bold text-blue-600" to="/login">
+        <Link className="font-bold text-blue-600" to="/">
           Login
         </Link>
       </p>
